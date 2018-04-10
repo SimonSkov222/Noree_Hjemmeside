10b10ce6cc1ae6344f41b5da8fcf1aae0696dc49
shsk <shsk@TEC-A-PC42.mshome.net>
2018-04-10 14:47:16 +0200

    commit

--- System/Networking/Routing.php
+++ System/Networking/Routing.php
@@ -1,8 +1,85 @@
 <?php
+namespace System\Networking;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use System\Utils\StringExt;
 
+Routing::Start();
+
+class Routing
+{
+    private static $routes;
+    
+    public static function Start()
+    {
+        echo 'Routing...';
+        
+        $uri = self::GetUri();
+        self::$routes = self::GetAllRoutes();
+        echo "myRoute: " .self::GetRoute($uri)."\n";
+        
+    }
+    
+    private static function GetUri()
+    {
+        $uri = filter_input(INPUT_GET, "Route");
+        if ($uri == "") 
+        {
+            $uri = "/Home";
+        }
+        
+        return $uri;
+    }
+    
+    private static function GetLang() {}
+    
+    private static function GetRoute($uri) 
+    {
+        $routeKeys = array_keys(self::$routes);
+        
+        print_r($routeKeys);
+        
+        $parts = explode("/", $uri);
+        $route = "";
+        $check = "";
+        
+        foreach ($parts as $part) 
+        {
+            if ($part = "") { continue; }
+            $check .= strtolower("/$part");
+            
+            for ($i = count($routeKeys); $i >= 0; $i--)
+            {
+                $keyLow = strtolower($routeKeys[$i]);
+                
+                if (!StringExt::StartsWith($keyLow, $check))
+                {
+                    unset($routeKeys[$i]);
+                }
+                
+                if ($keyLow == $check)
+                {
+                    $route = $routeKeys[$i];
+                }
+            }
+        }
+        
+        if ($route = "") 
+        {
+            $route = "/NotFound";
+        }
+        
+        return $route;
+    }
+    
+    private static function GetAllRoutes() 
+    {
+        $content = file_get_contents(__DIR__."/Routes.json");
+        $json = json_decode($content, true);
+        
+        print_r($json);
+        
+        return $json;
+    }
+    
+    
+}
