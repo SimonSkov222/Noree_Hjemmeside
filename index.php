25af1b216c94ea7e8b340852561d5142bf79b6f6
kide <kide@TEC-A-PC6.pc-tec.local>
2018-04-11 10:14:39 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-header("Content-Type: text/plain");
-require_once __DIR__."/System/Networking/Routing.php";
+header('Content-Type: application/javascript');
+
+echo "ss\n";
+
+
+require_once __DIR__."/System/Utils/Autoload.php";
+
+System\Utils\StringExt::Test();
+System\Database\MySQL::Update("MinTable", array("Name" => "Kim", "Age" => 12));
