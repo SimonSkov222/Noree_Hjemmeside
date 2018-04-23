7c3087f098f54415ddf3fd9889aaf096a8c0a87f
shsk <shsk@TEC-A-PC42.mshome.net>
2018-04-23 12:47:59 +0200

    Commit - Simon

--- System/Utils/ControllerBasic.php
+++ System/Utils/ControllerBasic.php
@@ -19,12 +19,12 @@ class ControllerBasic
     
     public function __destruct() 
     {
+        global $G_Template_Data;
         //print_r($_SERVER
         //
         $G_Template_Data["Debug"] = ob_get_contents();
         ob_end_clean();
         
-        global $G_Template_Data;
         $template = __Root."/Content/Template".__Template."/".$G_Template_Data["Design"].".php";
         require_once $template;
         
